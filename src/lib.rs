f404afaa39feb7ddba91a5b73e32e234cd7b4874
Will Lin <will.lin4@gmail.com>
2020-10-09 00:21:00 -0400

    Rework Absorbable and CryptographicSponge trait
    Absorbable is now sponge implementation independent and only
    requires conversion to a byte vector and a field element. The
    CryptographicSponge trait now supports outputting both bytes and
    field elements.

--- src/lib.rs
+++ src/lib.rs
@@ -17,48 +17,65 @@
 )]
 #![forbid(unsafe_code)]
 
-use ark_ff::Field;
+#[cfg(feature = "std")]
+extern crate std;
 
-/// An enum for specifying the output size.
+#[cfg(not(feature = "std"))]
+extern crate alloc as std;
+
+use ark_ff::Field;
+use std::vec::Vec;
+
+/// An enum for specifying an output size.
 pub enum OutputSize {
-    /// Sample outputs from the entire field.
+    /// Sample outputs from the entire output set.
     Full,
 
-    /// Sample outputs from a subset of the field.
+    /// Sample outputs from a subset of the output set.
     Truncated {
         /// The maximum size of the subset is 2^num_bits.
         num_bits: usize,
     },
 }
 
-/// An interface for objects that can be absorbed into a specified `CryptographicSponge`.
-pub trait Absorbable<F: Field, S: CryptographicSponge<F>> {
-    /// Converts the object into the input of the specified `CryptographicSponge`.
-    fn to_sponge_input(&self) -> S::Input;
+/// An interface for objects that can be absorbed by a `CryptographicSponge`.
+pub trait Absorbable<F: Field> {
+    /// Converts the object into a list of bytes that can be absorbed by a `CryptographicSponge`.
+    fn to_sponge_bytes(&self) -> Vec<u8>;
+
+    /// Converts the object into a field element that can be absorbed by a `CryptographicSponge`.
+    fn to_sponge_field_element(&self) -> F;
 }
 
 /// The interface for a sponge.
-/// A sponge can `absorb` or take in inputs and later `squeeze` or output field elements.
+/// A sponge can `absorb` or take in inputs and later `squeeze` or output bytes or field elements.
 /// The output depends on previous `absorb` and `squeeze` calls.
 pub trait CryptographicSponge<F: Field> {
-    /// The input type.
-    type Input;
-
     /// Initialize a new instance of the sponge.
     fn new() -> Self;
 
     /// Absorb an input.
-    fn absorb(&mut self, input: &impl Absorbable<F, Self>)
-    where
-        Self: Sized;
+    fn absorb(&mut self, input: &impl Absorbable<F>);
+
+    /// Output a list of bytes of full length.
+    /// The full length is implementation-dependent and the output is byte-aligned.
+    /// Depends on previous `absorb` and `squeeze` calls.
+    fn squeeze_bytes(&mut self) -> Vec<u8> {
+        self.squeeze_bytes_with_size(OutputSize::Full)
+    }
+
+    /// Output a list of bytes with a specified maximum length.
+    /// The output is byte-aligned.
+    /// Depends on previous `absorb` and `squeeze` calls.
+    fn squeeze_bytes_with_size(&mut self, size: OutputSize) -> Vec<u8>;
 
     /// Output a field element from the entire field.
     /// Depends on previous `absorb` and `squeeze` calls.
-    fn squeeze(&mut self) -> F {
-        self.squeeze_with_size(OutputSize::Full)
+    fn squeeze_field_element(&mut self) -> F {
+        self.squeeze_field_element_with_size(OutputSize::Full)
     }
 
     /// Output a field element from a subset of the field.
     /// Depends on previous `absorb` and `squeeze` calls.
-    fn squeeze_with_size(&mut self, size: OutputSize) -> F;
+    fn squeeze_field_element_with_size(&mut self, size: OutputSize) -> F;
 }
