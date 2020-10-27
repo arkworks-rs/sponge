9dc2f516fcafc9b2a50cff70b2200d0c7fa2f09c
Will Lin <will.lin4@gmail.com>
2020-10-27 00:23:40 -0400

    Add implementations for Absorbable

--- src/lib.rs
+++ src/lib.rs
@@ -25,17 +25,12 @@ extern crate std;
 #[macro_use]
 extern crate alloc as std;
 
-use ark_ff::Field;
+use ark_ff::models::{
+    Fp256, Fp256Parameters, Fp320, Fp320Parameters, Fp384, Fp384Parameters, Fp768, Fp768Parameters,
+    Fp832, Fp832Parameters,
+};
+use ark_ff::{to_bytes, PrimeField, ToConstraintField};
 use std::vec::Vec;
-
-/// An interface for objects that can be absorbed by a `CryptographicSponge`.
-pub trait Absorbable<F: Field> {
-    /// Converts the object into a list of bytes that can be absorbed by a `CryptographicSponge`.
-    fn to_sponge_bytes(&self) -> Vec<u8>;
-
-    /// Converts the object into field elements that can be absorbed by a `CryptographicSponge`.
-    fn to_sponge_field_elements(&self) -> Vec<F>;
-}
 
 /// An enum for specifying the output field element size.
 #[derive(Clone)]
@@ -53,7 +48,7 @@ pub enum FieldElementSize {
 /// The interface for a cryptographic sponge.
 /// A sponge can `absorb` or take in inputs and later `squeeze` or output bytes or field elements.
 /// The outputs are dependent on previous `absorb` and `squeeze` calls.
-pub trait CryptographicSponge<F: Field> {
+pub trait CryptographicSponge<F: PrimeField> {
     /// Initialize a new instance of the sponge.
     fn new() -> Self;
 
@@ -72,5 +67,153 @@ pub trait CryptographicSponge<F: Field> {
         self.squeeze_field_elements_with_sizes(
             vec![FieldElementSize::Full; num_elements].as_slice(),
         )
+    }
+}
+
+/// An interface for objects that can be absorbed by a `CryptographicSponge`.
+pub trait Absorbable<F: PrimeField> {
+    /// Converts the object into a list of bytes that can be absorbed by a `CryptographicSponge`.
+    fn to_sponge_bytes(&self) -> Vec<u8>;
+
+    /// Converts the object into field elements that can be absorbed by a `CryptographicSponge`.
+    fn to_sponge_field_elements(&self) -> Vec<F>;
+}
+
+impl<F: PrimeField> Absorbable<F> for u8 {
+    fn to_sponge_bytes(&self) -> Vec<u8> {
+        vec![self.clone()]
+    }
+
+    fn to_sponge_field_elements(&self) -> Vec<F> {
+        vec![F::from(self.clone())]
+    }
+}
+
+impl<F: PrimeField> Absorbable<F> for Vec<u8> {
+    fn to_sponge_bytes(&self) -> Vec<u8> {
+        self.clone()
+    }
+
+    fn to_sponge_field_elements(&self) -> Vec<F> {
+        self.to_field_elements().unwrap()
+    }
+}
+
+impl<F: PrimeField> Absorbable<F> for [u8] {
+    fn to_sponge_bytes(&self) -> Vec<u8> {
+        self.to_vec()
+    }
+
+    fn to_sponge_field_elements(&self) -> Vec<F> {
+        self.to_field_elements().unwrap()
+    }
+}
+
+macro_rules! impl_absorbable_field {
+    ($field:ident, $params:ident) => {
+        impl<P: $params> Absorbable<$field<P>> for $field<P> {
+            fn to_sponge_bytes(&self) -> Vec<u8> {
+                to_bytes![self].unwrap()
+            }
+
+            fn to_sponge_field_elements(&self) -> Vec<$field<P>> {
+                vec![self.clone()]
+            }
+        }
+
+        impl<P: $params> Absorbable<$field<P>> for Vec<$field<P>> {
+            fn to_sponge_bytes(&self) -> Vec<u8> {
+                to_bytes![self].unwrap()
+            }
+
+            fn to_sponge_field_elements(&self) -> Vec<$field<P>> {
+                self.clone()
+            }
+        }
+
+        impl<P: $params> Absorbable<$field<P>> for [$field<P>] {
+            fn to_sponge_bytes(&self) -> Vec<u8> {
+                to_bytes![self].unwrap()
+            }
+
+            fn to_sponge_field_elements(&self) -> Vec<$field<P>> {
+                self.to_vec().clone()
+            }
+        }
+    };
+}
+
+impl_absorbable_field!(Fp256, Fp256Parameters);
+impl_absorbable_field!(Fp320, Fp320Parameters);
+impl_absorbable_field!(Fp384, Fp384Parameters);
+impl_absorbable_field!(Fp768, Fp768Parameters);
+impl_absorbable_field!(Fp832, Fp832Parameters);
+
+macro_rules! impl_absorbable_unsigned {
+    ($t:ident) => {
+        impl<F: PrimeField> Absorbable<F> for $t {
+            fn to_sponge_bytes(&self) -> Vec<u8> {
+                self.to_le_bytes().to_vec()
+            }
+
+            fn to_sponge_field_elements(&self) -> Vec<F> {
+                vec![F::from(self.clone())]
+            }
+        }
+    };
+}
+
+impl_absorbable_unsigned!(u16);
+impl_absorbable_unsigned!(u32);
+impl_absorbable_unsigned!(u64);
+impl_absorbable_unsigned!(u128);
+
+macro_rules! impl_absorbable_to_le_bytes {
+    ($t:ident) => {
+        impl<F: PrimeField> Absorbable<F> for $t {
+            fn to_sponge_bytes(&self) -> Vec<u8> {
+                self.to_le_bytes().to_vec()
+            }
+
+            fn to_sponge_field_elements(&self) -> Vec<F> {
+                self.to_le_bytes().to_sponge_field_elements()
+            }
+        }
+    };
+}
+
+impl_absorbable_to_le_bytes!(i8);
+impl_absorbable_to_le_bytes!(i16);
+impl_absorbable_to_le_bytes!(i32);
+impl_absorbable_to_le_bytes!(i64);
+impl_absorbable_to_le_bytes!(i128);
+impl_absorbable_to_le_bytes!(isize);
+impl_absorbable_to_le_bytes!(usize);
+
+impl<F: PrimeField> Absorbable<F> for bool {
+    fn to_sponge_bytes(&self) -> Vec<u8> {
+        vec![(self.clone() as u8)]
+    }
+
+    fn to_sponge_field_elements(&self) -> Vec<F> {
+        vec![F::from(self.clone())]
+    }
+}
+
+impl<F: PrimeField, A: Absorbable<F>> Absorbable<F> for Option<A> {
+    fn to_sponge_bytes(&self) -> Vec<u8> {
+        let mut output = vec![(self.is_some()) as u8];
+        if let Some(absorbable) = self {
+            output.extend(absorbable.to_sponge_bytes());
+        };
+        output
+    }
+
+    fn to_sponge_field_elements(&self) -> Vec<F> {
+        let mut output = vec![F::from((self.is_some()) as u8)];
+        if let Some(absorbable) = self {
+            output.extend(absorbable.to_sponge_field_elements());
+        };
+        output
     }
 }
