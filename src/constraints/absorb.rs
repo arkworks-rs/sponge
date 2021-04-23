415f5419eca5d7ffae154b2e6c29f8401c3b5f95
Tom Shen <tomshen@berkeley.edu>
2021-04-23 14:24:21 -0700

    fix r1cs + no_std

--- src/constraints/absorb.rs
+++ src/constraints/absorb.rs
@@ -11,7 +11,7 @@ use ark_r1cs_std::groups::curves::twisted_edwards::AffineVar as TEAffineVar;
 use ark_r1cs_std::ToConstraintFieldGadget;
 use ark_relations::r1cs::SynthesisError;
 use ark_std::vec::Vec;
-
+use ark_std::vec;
 /// An interface for objects that can be absorbed by a `CryptographicSpongeVar`.
 pub trait AbsorbGadget<F: PrimeField> {
     /// Converts the object into field elements that can be absorbed by a `CryptographicSpongeVar`.
